9cfc82eef26149dc2b06c0f2176f5f78673eb7d0
bim pham <90117142+lenaphamthi@users.noreply.github.com>
2022-11-19 09:28:05 +0700

    Add files via upload

---
+++ Chuong5_HuanLuyenMoHinh&Loi/ex4.1-4.2_HuanluyenmohinhHQTT/ex4.1-BT01.py
@@ -0,0 +1,12 @@
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+
+D = np.loadtxt(os.path.join("ex1data2.txt"),  delimiter=",")
+X, y = D[:,:-1], D[:,-1]
+print('Kích thước dữ liệu gốc:')
+print("\t\tX: ", X.shape, "; y: ", y.shape)
+X_train, X_test, y_train, y_test = train_test_split(X,  y,test_size=0.30, random_state=15)
+print('Kích thước dữ liệu train - test:')
+print("\t\tX train: ", X_train.shape, "; y train: ",  y_train.shape)
+print("\t\tX test: ", X_test.shape, "; y test: ",  y_test.shape)
