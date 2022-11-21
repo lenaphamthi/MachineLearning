5f220d6a5ed64d66d632aeaf7dfad55c2d151b87
bim pham <90117142+lenaphamthi@users.noreply.github.com>
2022-11-21 13:36:43 +0700

    Add files via upload

--- Chuong5_HuanLuyenMoHinh&Loi/BTKN01-Train_Test.py
+++ Chuong5_HuanLuyenMoHinh&Loi/BTKN01-Train_Test.py
@@ -1,11 +1,25 @@
-import os
+from asyncore import read
 import numpy as np
-from sklearn.model_selection import train_test_split
-D = np.loadtxt(os.path.join("ex1data2.txt"),delimiter=",")
-X, y = D[:,:-1], D[:,-1]
-print('Kích thước dữ liệu gốc:')
-print("\t\tX: ", X.shape, "; y: ", y.shape)
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.30, random_state=15)
-print('Kích thước dữ liệu train - test:')
-print("\t\tX train: ", X_train.shape, "; y train: ",y_train.shape)
-print("\t\tX test: ", X_test.shape, "; y test: ",y_test.shape)
+
+def readData(filename,folder=""):
+    data = np.loadtxt(os.path.join(folder,filename),delimiter=',')
+    X = data[:, :-1]
+    y = data[:, -1]
+    return X, y
+
+def split_train_test(X, y, test_size, random_state = None):
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state = random_state)
+    return X_train, X_test, y_train, y_test
+
+def print_train_test(X_train, X_test, y_train, y_test):
+    print(f"Size of training set\nX train: {X_train.shape} and y train: {y_train.shape}")
+    print(f"Size of test set\nX test: {X_test.shape} and y test: {y_test.shape}")
+
+def main():
+    X, y = read_txt("ex1data2.txt", delimiter=',')
+    X_train, X_test, y_train, y_test =  split_train_test(X, y, test_size = 0.3, random_state=4)
+    print_train_test(X_train, X_test, y_train, y_test)
+
+if __name__ == "__main__":
+    main()
